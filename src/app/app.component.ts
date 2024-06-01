7a85de191678f4d641ed7c20846b61a93901cf91
gitmbints <rahmbints@gmail.com>
2024-06-01 19:10:38 +0300

    Fix the bug: redirection to the login page when the page is refreshed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { AuthService } from "./auth.service";
 
 @Component({
 	selector: "app-root",
 	templateUrl: "./app.component.html",
 	styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-	constructor(private router: Router) {}
+export class AppComponent implements OnInit {
+  auth: AuthService;
+
+  constructor(private authService: AuthService, private router: Router) { }
+  
+  ngOnInit() {
+    this.auth = this.authService;
+  }
 
 	goToPokemonList() {
 		this.router.navigate(["/pokemons"]);
-	}
+  }
+  
+  logout() {
+    this.auth.logout();
+    this.router.navigate(["/login"]);
+  }
 }
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -8,8 +8,16 @@ export class AuthService {
 	isLoggedIn: boolean = false;
 	redirectUrl: string;
 
+	constructor() {
+		this.isLoggedIn = this.checkLoggedIn();
+	}
+
 	login(name: string, password: string): Observable<boolean> {
-		const isLoggedIn = name == "pikachu" && password == "pikachu";
+		const isLoggedIn = name === "pikachu" && password === "pikachu";
+
+		if (isLoggedIn) {
+			localStorage.setItem('isLoggedIn', 'true');
+		}
 
 		return of(isLoggedIn).pipe(
 			delay(1000),
@@ -18,6 +26,11 @@ export class AuthService {
 	}
 
 	logout() {
+		localStorage.removeItem('isLoggedIn');
 		this.isLoggedIn = false;
+	}
+
+	private checkLoggedIn(): boolean {
+		return localStorage.getItem('isLoggedIn') === 'true';
 	}
 }
