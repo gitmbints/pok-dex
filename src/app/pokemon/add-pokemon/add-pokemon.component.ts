e15d5e50a1139c2a0c0cd2843c793f7cbd9e8a54
gitmbints <rahmbints@gmail.com>
2024-06-01 19:32:09 +0300

    Fix logout btn

--- src/app/pokemon/add-pokemon/add-pokemon.component.ts
+++ src/app/pokemon/add-pokemon/add-pokemon.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from "@angular/core";
+import { FormsModule } from '@angular/forms';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 import { Pokemon } from "../pokemon";
 
 @Component({
 	selector: "app-add-pokemon",
-	template: `
-		<h2 class="center">Ajouter un pokémon</h2>
-		<app-pokemon-form [pokemon]="pokemon"></app-pokemon-form>
+  template: `
+    <div class="add-pokemon-form">
+      <h1>Ajouter un pokémon</h1>
+      <app-pokemon-form [pokemon]="pokemon"></app-pokemon-form>
+    </div>
 	`,
+  styleUrls: ["./add-pokemon.component.css"],
+	encapsulation: ViewEncapsulation.None,
 })
 export class AddPokemonComponent implements OnInit {
 	pokemon: Pokemon;
